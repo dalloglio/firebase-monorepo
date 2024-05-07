2196d943a0aefd92d13cdaa5a1807daf43e98e96
Ricardo Pires <ricardo.tech@live.com>
2024-05-07 13:15:25 -0300

    chore: remove trigger service

--- apps/trigger-service/src/index.ts
+++
@@ -1,7 +0,0 @@
-import * as functions from "firebase-functions/v1";
-
-export const userCreated = functions.firestore
-  .document("users/{userId}")
-  .onCreate((snap) => {
-    return snap.ref.set({incrementId: 1}, {merge: true});
-  });
